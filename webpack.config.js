452bac2583fb6ae36ee68a799a67f4bae24006ec
kylewong975 <kyle.wong975@gmail.com>
2017-12-20 21:50:59 -0800

    Frontend setup with webpack, babel, etc

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+let path = require('path');
+let webpack = require('webpack');
+let BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+	// base directory for resolving entry
+	context: __dirname,
+	// entry point
+	entry: './assets/js/index',
+	// compiled bundle, location and naming convention
+	output: {
+		path: path.resolve('./assets/bundles/'),
+		filename: '[name]-[hash].js',
+	},
+	// plugins for the website
+	plugins: [
+		// tell webpack where to store data about the bundle
+		new BundleTracker({
+			filename: './webpack-stats.json'
+		}),
+		// make jQuery available in every module
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+			'window.jQuery': 'jquery'
+		})
+	],
+	module: {
+		// instruct webpack to use the following loaders on all js/jsx files
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/, // exclude node_modules when asking babel to transpile
+				loader: 'babel-loader', // use babel loader
+				query: {
+					presets: ['react'] // using React
+				}
+			}
+		]
+	},
+	resolve: {
+		modulesDirectories: ['node_modules'], // look for modules at node_modules
+		extensions: ['', '.js', '.jsx'] // these are the extensions to be used to resolve modules
+	},
+};
